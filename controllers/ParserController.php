4629bbfedac1fd4a7f737aa918f0aa9167d698a9
Max <maxim.gavrilenko@pdffiller.com>
2015-08-19 14:33:01 +0300

    minifix

--- controllers/ParserController.php
+++ controllers/ParserController.php
@@ -6,12 +6,15 @@ use yii\db\Exception;
 
 use maxlen\parser\helpers\Parser;
 use maxlen\parser\models\ParserLinks;
+use maxlen\proxy\helpers\Proxy;
 
 class ParserController extends Controller
 {
    public function actionGrabLinks($domain, $linkId) {
        $params = Parser::getParams();
        $params['domain'] = $domain;
+
+       Proxy::getRandomProxy();
 
        $link = ParserLinks::find()->where(['id' => $linkId])->limit(1)->one();
 
